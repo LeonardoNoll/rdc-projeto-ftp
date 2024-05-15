6942d748e641594b19aa9515d7437eaecc347b43
Leonardo Noll Cruz <leonoll8@hotmail.com>
2024-05-15 17:06:49 -0300

    adição de comentarios

--- Client/client.py
+++ Client/client.py
@@ -1,27 +1,27 @@
 import socket
 
-# HOST = '127.0.0.1'
+# Pede para o cliente inserir o IP do servidor
 HOST = str(input('Digite o IP do servidor>'))
 PORT = 5000
 
-
-
+# Cria socket e conecta ao servidor
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
+# Conexão estabelecida
 print('Conectado em: {HOST}\n')
 
+# Pede para o cliente inserir o nome do arquivo a buscar
 namefile = str(input('Buscar arquivo>'))
-
 client.send(namefile.encode())
 
+# Recebe o arquivo
 with open("Client/"+namefile, 'wb') as file:
     while 1:
         data = client.recv(1000000)
         if not data:
             break
         file.write(data)
-
     print('Arquivo recebido')
 
  
--- Server/server.py
+++ Server/server.py
@@ -1,35 +1,31 @@
 import socket
 
-# HOST = '192.168.1.9'
 PORT = 5000
 
+# Busca e mostra o IP do servidor
 hostname = socket.gethostname()
 HOST = socket.gethostbyname(hostname)
-print(f"Hostname: {hostname}")
 print(f"IP Address: {HOST}")
 
+# Cria socket e vincula o endereço e a porta
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-server.listen(1)
 
+# Escuta a porta
+server.listen(1)
 print ('Esperando o cliente no servidor: ', HOST,'e porta: ', PORT)
 connection, adress = server.accept()
 
+# Conexão estabelecida
+# Mostra o endereço do cliente
 print ('Conectado em: ', adress)
 
 namefile = connection.recv(1024).decode()
 
+# Envia o arquivo
 with open("Server/"+namefile, 'rb') as file:
     for data in file.readlines():
         connection.send(data)
-    
     print('Arquivo enviado!')
 
-# while True:
-#     data = conn.recv(1024)
-#     if not data:
-#         print ('Fechando a conexao')
-#         conn.close()
-#         break
-#     conn.sendall(data)
 
