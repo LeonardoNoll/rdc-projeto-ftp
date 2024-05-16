8aedfef8bee7462f081c9341c09237d537aa7703
Leonardo Noll Cruz <leonoll8@hotmail.com>
2024-05-16 16:14:53 -0300

    pre reforma com chatgpt

--- Client/client.py
+++ Client/client.py
@@ -1,27 +1,50 @@
 import socket
 
 # Pede para o cliente inserir o IP do servidor
-HOST = str(input('Digite o IP do servidor>'))
+# HOST = str(input('Digite o IP do servidor>'))
+HOST = '192.168.1.11'
 PORT = 5000
 
+def get_file():
+    # Pede para o cliente inserir o nome do arquivo a buscar
+    namefile = str(input('Buscar arquivo>'))
+    connection.send(namefile.encode())
+
+    # Recebe o arquivo
+    with open("Client/"+namefile, 'wb') as file:
+        while 1:
+            data = connection.recv(1000000)
+            if not data:
+                break
+            file.write(data)
+        print('Arquivo recebido')
+
+def send_file():
+    # Pede para o cliente inserir o nome do arquivo a enviar
+    namefile = str(input('Enviar arquivo>'))
+    connection.send(namefile.encode())
+
+    # Envia o arquivo
+    with open("Client/"+namefile, 'rb') as file:
+        data = file.read(1024)
+        while data:
+            connection.send(data)
+            data = file.read(1024)
+        connection.send(b'')
+        print('Arquivo enviado!')
+        
+
+
+
+
 # Cria socket e conecta ao servidor
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect((HOST, PORT))
 
 # Conexão estabelecida
 print('Conectado em: {HOST}\n')
 
-# Pede para o cliente inserir o nome do arquivo a buscar
-namefile = str(input('Buscar arquivo>'))
-client.send(namefile.encode())
-
-# Recebe o arquivo
-with open("Client/"+namefile, 'wb') as file:
-    while 1:
-        data = client.recv(1000000)
-        if not data:
-            break
-        file.write(data)
-    print('Arquivo recebido')
+send_file()
+get_file()
 
  
--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,35 @@
 import socket
 
-PORT = 5000
-
-# Busca e mostra o IP do servidor
 hostname = socket.gethostname()
 HOST = socket.gethostbyname(hostname)
-print(f"IP Address: {HOST}")
+PORT = 5000
+print('IP do servidor: ', HOST)
+
+def receive_file():
+    print('Recebendo arquivo...')
+    namefile = connection.recv(1024).decode()
+    print(namefile)
+    with open("Server/"+namefile, 'wb') as file:
+        print('Arquivo aberto...')
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            file.write(data)
+        print('Arquivo recebido!')
+
+def send_file():
+    namefile = connection.recv(1024).decode()
+    with open("Server/"+namefile, 'rb') as file:
+        data = file.read(1024)
+        while data:
+            connection.send(data)
+            data = file.read(1024)
+        print('Arquivo enviado!')
+
+
+
+# Busca e mostra o IP do servidor
 
 # Cria socket e vincula o endereço e a porta
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,12 +44,10 @@ connection, adress = server.accept()
 # Mostra o endereço do cliente
 print ('Conectado em: ', adress)
 
-namefile = connection.recv(1024).decode()
+# namefile = connection.recv(1024).decode()
 
 # Envia o arquivo
-with open("Server/"+namefile, 'rb') as file:
-    for data in file.readlines():
-        connection.send(data)
-    print('Arquivo enviado!')
+receive_file()
+send_file()
 
 
